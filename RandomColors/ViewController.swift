e1c714d81796d1e5a2f50f74a8b0664b7983eb3b
Maggie H <hillebrecht@chapman.edu>
2024-08-03 17:24:16 -0700

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Maggie Hillebrecht on 8/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
